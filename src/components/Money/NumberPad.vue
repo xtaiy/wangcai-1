3f17b38ef9b0340ce0c98a7815eef2c5031c3706
taiy <1726311422@qq.com>
2022-05-21 17:10:51 +0800

    添加 createdAt 字段

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -59,7 +59,9 @@ export default class NumberPad extends Vue{
     this.output='0';
   }
   ok(){
-    this.$emit('update:value',this.output)
+    this.$emit('update:value',this.output);
+    this.$emit('submit',this.output);
+    this.output='0';
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      <NumberPad @update:value="onUpdateAmount"/>
+      {{recordList}}
+      <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
       <Types :value.sync="record.type"/>
       <Notes @update:value="onUpdateNotes"/>
       <Tags :dataSource.sync="tags" @update:value="onUpdateTags"/>
@@ -18,13 +19,27 @@ import Tags from '@/components/Money/Tags.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Types from '@/components/Money/Types.vue';
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
+
+
+// const version=window.localStorage.getItem('version');
+const recordList:Record[]=JSON.parse(window.localStorage.getItem('recordList')||'[]');
+// if(version==='0..0.1'){
+//   //数据库升级，数据迁移
+//   recordList.forEach(record=>{
+//     record.createdAt=new Date(2022,5,1);
+//   })
+//   //保存数据
+//   window.localStorage.setItem('recordList',JSON.stringify(recordList))
+// }
+// window.localStorage.setItem('version','0.0.2');
 
 type Record={
   tags:string[]
   notes:string
   type:string
   amount:number
+  createdAt?:Date
 }
 @Component({
   components: {Types, Notes, Tags, NumberPad}
@@ -32,6 +47,7 @@ type Record={
 export default class Money extends Vue{
   tags=['衣','食','住','行'];
   record:Record={tags:[],notes:'',type:'-',amount:0}
+  recordList:Record[]=recordList;
   onUpdateTags(value:string[]){
     this.record.tags=value;
   }
@@ -40,6 +56,15 @@ export default class Money extends Vue{
   }
   onUpdateAmount(value:string){
     this.record.amount=parseFloat(value);
+  }
+  saveRecord(){
+    const record2:Record=JSON.parse(JSON.stringify(this.record));
+    record2.createdAt=new Date();
+    this.recordList.push(record2)
+  }
+  @Watch('recordList')
+  onRecordChange(){
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
   }
 }
 </script>
