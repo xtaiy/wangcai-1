23cb6be431515b5b60529026bf4ad3362aa18daa
taiy <1726311422@qq.com>
2022-06-03 14:13:15 +0800

    value从data变成了prop

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,7 +2,8 @@
   <Layout>
     <div class="tags">
       <router-link :to="`/labels/edit/${tag.id}`" class="tag" v-for="tag in tags" :key="tag.id"><span>{{tag.name}}</span>
-      <Icon name="right"></Icon></router-link>
+      <Icon name="right"></Icon>
+      </router-link>
     </div>
     <div class="createTag-wrapper">
       <Button @click="createTag">新建标签</Button>
