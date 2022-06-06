adb398cb111a1cf7f73dea219aa4174ba203692a
taiy <1726311422@qq.com>
2022-06-06 21:20:51 +0800

    保证用户用手机打开页面，否则就提示用户

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# abort on errors
 set -e
 
-# 构建
+# build
 yarn build
 
-# cd 到构建输出的目录下
+# navigate into the build output directory
 cd dist
 
-# 部署到自定义域域名
+# if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xtaiy/waicai-website.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+ git push -f git@github.com:xtaiy/wangcai-1website.git main:gh-pages
 
 cd -
